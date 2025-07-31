a57d86fab71dec498e40e945f18dfadd5df346f9
junsik <junsik@ma.il>
2025-07-31 12:54:58 +0900

    [feature] p.35 인수분해 3 case 추가

--- test_prime_factors.py
+++ test_prime_factors.py
@@ -1,12 +1,16 @@
-
 from prime_factors import PrimeFactor
 
 
 def test_prime_factors_of_1():
     prime_factor = PrimeFactor()
-    assert  prime_factor.of(1) == []
+    assert prime_factor.of(1) == []
 
 
 def test_prime_factors_of_2():
     prime_factor = PrimeFactor()
-    assert  prime_factor.of(2) == [2]
+    assert prime_factor.of(2) == [2]
+
+
+def test_prime_factors_of_3():
+    prime_factor = PrimeFactor()
+    assert prime_factor.of(3) == [3]
